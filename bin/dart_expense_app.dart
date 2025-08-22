59044f8c62613114e4d7d76bf341ec7d6a1ee451
jayncheta <6631501185@lamduan.mfu.ac.th>
2025-08-22 11:40:06 +0700

    Complete two features: showAll and totalPaid gay

--- bin/dart_expense_app.dart
+++ bin/dart_expense_app.dart
@@ -1,12 +1,13 @@
 void showAll() {
-  
+  print('1. Lunch: 50 bhat');
+  print('2. Coffee: 70 bhat');
 }
 
 void totalPaid() {
-  
+  print('Total paid: 120 bhat');
 }
 
-void main(){
+void main() {
   showAll();
   totalPaid();
 }
